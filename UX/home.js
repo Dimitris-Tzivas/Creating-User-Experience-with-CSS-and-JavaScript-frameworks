c1a4546fa72d49dc8449b3fb610bf87d6b2942b2
Dimitris-Tzivas <dimitristzivas1@gmail.com>
2023-02-17 13:43:54 +0200

    u15

--- UX/home.js
+++ UX/home.js
@@ -1,17 +1,17 @@
-const myLink = document.getElementById("my-link");
+// const myLink = document.getElementById("my-link");
 
-myLink.addEventListener("click", function (event) {
-  // event.preventDefault(); // prevent the default behavior of following the link
-  // const { exec } = require("child_process");
-  // const path =
-  //   "C:UsersdimitGitHubCreating-User-Experience-with-CSS-and-JavaScript-frameworksAdmin Dashboardindex.js";
-  // exec("node index.js", { cwd: path }, (err, stdout, stderr) => {
-  //   if (err) {
-  //     console.error(`Error: ${err}`);
-  //     return;
-  //   }
-  //   console.log(`Output: ${stdout}`);
-  // });
-  // event.preventDefault(); // prevent the default behavior of following the link
-  console.log("123"); // log "123" when the link is clicked
-});
+// myLink.addEventListener("click", function (event) {
+//   // event.preventDefault(); // prevent the default behavior of following the link
+//   // const { exec } = require("child_process");
+//   // const path =
+//   //   "C:UsersdimitGitHubCreating-User-Experience-with-CSS-and-JavaScript-frameworksAdmin Dashboardindex.js";
+//   // exec("node index.js", { cwd: path }, (err, stdout, stderr) => {
+//   //   if (err) {
+//   //     console.error(`Error: ${err}`);
+//   //     return;
+//   //   }
+//   //   console.log(`Output: ${stdout}`);
+//   // });
+//   // event.preventDefault(); // prevent the default behavior of following the link
+//   console.log("123"); // log "123" when the link is clicked
+// });
