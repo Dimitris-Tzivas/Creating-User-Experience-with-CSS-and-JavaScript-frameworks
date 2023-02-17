6c51fd78c34e535eae8de1ea61bb4b16ddddd189
Dimitris-Tzivas <dimitristzivas1@gmail.com>
2023-02-17 13:25:36 +0200

    u8

--- UX/home.js
+++ UX/home.js
@@ -3,15 +3,15 @@ const myLink = document.getElementById("my-link");
 myLink.addEventListener("click", function () {
   console.log("4654654654654");
   //   event.preventDefault(); // prevent the default behavior of following the link
-  const { exec } = require("child_process");
-  const path =
-    "C:UsersdimitGitHubCreating-User-Experience-with-CSS-and-JavaScript-frameworksAdmin Dashboardindex.js";
+  // const { exec } = require("child_process");
+  // const path =
+  //   "C:UsersdimitGitHubCreating-User-Experience-with-CSS-and-JavaScript-frameworksAdmin Dashboardindex.js";
 
-  exec("node index.js", { cwd: path }, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error: ${err}`);
-      return;
-    }
-    console.log(`Output: ${stdout}`);
-  });
+  // exec("node index.js", { cwd: path }, (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.error(`Error: ${err}`);
+  //     return;
+  //   }
+  //   console.log(`Output: ${stdout}`);
+  // });
 });
